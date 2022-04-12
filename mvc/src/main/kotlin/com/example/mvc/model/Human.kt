cf9093db6cb56f7f481f8c5182a05184b9a62f33
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-04-12 15:56:12 +0900

    v1
    getmapping

---
+++ mvc/src/main/kotlin/com/example/mvc/model/Human.kt
@@ -0,0 +1,11 @@
+package com.example.mvc.model
+
+//DTO
+data class Human(var name:String?=null, var age:String?=null, var address:String?=null){
+/*
+    var name:String?=null
+    var age:String?=null
+    var address:String?=null*/
+
+
+}
